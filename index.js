c873a99a4f8724144e05b5529c92abe68733977c
Michael Nwogu <mikedbchi@gmail.com>
2021-07-06 14:44:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function actualDate(dt)
+  {
+    return dt.getDate()+(dt.getDate() % 10 == 1 && dt.getDate() != 11 ? 'st' : (dt.getDate() % 10 == 2 && dt.getDate() != 12 ? 'nd' : (dt.getDate() % 10 == 3 && dt.getDate() != 13 ? 'rd' : 'th'))); 
+  }
+
+dt = new Date();
+console.log(actualDate(dt));
